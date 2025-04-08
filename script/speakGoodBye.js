2da42c4aff36beb41da235ce973d67b6c49763d6
saraserrar <serrarsara25@gmail.com>
2025-04-09 00:03:03 +0100

    Update speakGoodBye.js

--- script/speakGoodBye.js
+++ script/speakGoodBye.js
@@ -1,1 +1,9 @@
 
+(function(window){
+    var goodbye="Goodbye";
+    var byespeaker={};
+    byespeaker.saygoodbye=function(names){
+        console.log(goodbye+" "+names);
+    }
+    window.byespeaker=byespeaker;
+})(window);
